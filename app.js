02e7f257efadf09687bc90ef27ab8ea06f8b047f
WFrancois <wroblewski.francois@gmail.com>
2017-12-30 15:29:51 +0100

    Remove updatetry

--- app.js
+++ app.js
@@ -60,11 +60,6 @@ client.on("chat", function (channel, userstate, message, self) {
                 client.say(channel, messageToSend);
             }).catch(console.warn);
             break;
-        case '!updatetry':
-            if(userstate.mod) {
-                client.say(channel, '!editcom !try $(touser) > Best try Argus MM: P3 ' + command.target + '%');
-            }
-            break;
     }
 });
 
