d3dc7fa02bfa2d329fd74e7dc95cf99521ea9486
WFrancois <wroblewski.francois@gmail.com>
2018-01-10 22:52:19 +0100

    Doesnt write music if stream isn't online

--- commands/music.js
+++ commands/music.js
@@ -1,18 +1,25 @@
 const config = require('config');
+const twitchApi = require('../libs/twitch.js');
 const {Client} = require('pg');
 
 module.exports.getMessage = function (channel) {
     return new Promise((resolve, reject) => {
-        const client = new Client(config.get('db.music'));
-        client.connect();
-
-        client.query('SELECT * FROM current_music WHERE channel = $1', [channel]).then(function (rows) {
-            client.end();
-            if (rows && rows.rows && rows.rows[0]) {
-                return resolve(rows.rows[0].title)
-            } else {
-                return reject('not-found');
+        twitchApi.isOnline(channel).then((online) => {
+            if(!online) {
+                return resolve('stream not online :(');
             }
-        }).catch(reject);
+
+            const client = new Client(config.get('db.music'));
+            client.connect();
+
+            client.query('SELECT * FROM current_music WHERE channel = $1', [channel]).then(function (rows) {
+                client.end();
+                if (rows && rows.rows && rows.rows[0]) {
+                    return resolve(rows.rows[0].title)
+                } else {
+                    return reject('not-found');
+                }
+            }).catch(reject);
+        });
     });
 };
