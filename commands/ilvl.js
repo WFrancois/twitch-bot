bee5948850d4be5a467c1876e040d2635bfab868
WFrancois <wroblewski.francois@gmail.com>
2018-08-20 23:24:35 +0200

    Display azerite item level

--- commands/ilvl.js
+++ commands/ilvl.js
@@ -10,7 +10,7 @@ module.exports.getMessage = function (location) {
             realm: 'ysondre',
             name: 'lapintade'
         }).then(response => {
-            resolve('Ilvl de Lapi équipé : ' + response.data.items.averageItemLevelEquipped + ' (en sac : ' + response.data.items.averageItemLevel + ')');
+            resolve('Ilvl de Lapi équipé : ' + response.data.items.averageItemLevelEquipped + ' (en sac : ' + response.data.items.averageItemLevel + ') ; Azerite Item Level : ' + response.data.items.neck.azeriteItem.azeriteLevel);
         }).catch(reject);
     });
 };
