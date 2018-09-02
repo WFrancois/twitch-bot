57ce873c5d932166701665da06b8ec3558d409bb
WFrancois <wroblewski.francois@gmail.com>
2018-09-02 17:55:53 +0200

    Add timezone

--- commands/music.js
+++ commands/music.js
@@ -12,7 +12,7 @@ module.exports.getMessage = function (channel) {
             const client = new Client(config.get('db.music'));
             client.connect();
 
-            client.query('SELECT * FROM current_music WHERE channel = $1 AND last_update > NOW() - INTERVAL \'30 second\'', [channel]).then(function (rows) {
+            client.query('SELECT * FROM current_music WHERE channel = $1 AND last_update > (NOW() AT TIME ZONE \'Europe/Paris\') - INTERVAL \'30 second\'', [channel]).then(function (rows) {
                 client.end();
                 if (rows && rows.rows && rows.rows[0]) {
                     return resolve(rows.rows[0].title)
