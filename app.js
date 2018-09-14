2cb24d953d4d72d1de17903d5e7c95a0dfb6c654
WFrancois <wroblewski.francois@gmail.com>
2018-09-14 14:50:36 +0200

    Disable try until it has been rework

--- app.js
+++ app.js
@@ -164,17 +164,17 @@ client.on("chat", function (channel, userstate, message, self) {
                 client.say(channel, messageToSend);
             }).catch(console.warn);
             break;
-        case '!try':
-            if (!serviceCommand.canUseCommand('try')) {
-                break;
-            }
-            serviceCommand.useCommand('try');
-
-            commandTwitch.warcraftlogs.getMessage().then(messageToSend => {
-                messageToSend = command.target + ' > Ce soir : ' + messageToSend;
-                client.say(channel, messageToSend);
-            }).catch(console.warn);
-            break;
+        // case '!try':
+        //     if (!serviceCommand.canUseCommand('try')) {
+        //         break;
+        //     }
+        //     serviceCommand.useCommand('try');
+        //
+        //     commandTwitch.warcraftlogs.getMessage().then(messageToSend => {
+        //         messageToSend = command.target + ' > Ce soir : ' + messageToSend;
+        //         client.say(channel, messageToSend);
+        //     }).catch(console.warn);
+        //     break;
     }
 });
 
