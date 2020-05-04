72c6b8bb31669302e669169fb98177f75510d0e4
WFrancois <wroblewski.francois@gmail.com>
2020-05-04 21:12:06 +0200

    Fix isonline function

--- libs/twitch.js
+++ libs/twitch.js
@@ -1,20 +1,44 @@
 const config = require('config');
 const request = require('request');
 
-module.exports.isOnline = function(channelName) {
-    return new Promise((resolve, reject) => {
-        request({
-            url: 'https://api.twitch.tv/helix/streams?user_login=' + channelName,
-            headers: {
-                'Client-ID': config.get('twitch.client_id')
-            },
-            json: true
-        }, function(error, response, body) {
-            if(error) {
-                return reject(error);
+module.exports.isOnline = async function(channelName) {
+    const accessToken = await getAppToken();
+
+    const {body} = await tempRequestAsync({
+        url: 'https://api.twitch.tv/helix/streams?user_login=' + channelName,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Client-ID': config.get('twitch.client_id'),
+        },
+        json: true
+    });
+
+    return !!body.data[0];
+};
+
+const getAppToken = async function () {
+    const {body} = await tempRequestAsync({
+        url: 'https://id.twitch.tv/oauth2/token',
+        method: 'POST',
+        qs: {
+            client_id: config.get('twitch.client_id'),
+            client_secret: config.get('twitch.client_secret'),
+            grant_type: 'client_credentials',
+        },
+        json: true
+    });
+
+    return body.access_token;
+};
+
+function tempRequestAsync (requestConfig) {
+    return new Promise(((resolve, reject) => {
+        request(requestConfig, (error, response, body) => {
+            if (error) {
+                reject(error);
             }
 
-            resolve(!!body.data[0]);
-        });
-    });
-};
+            resolve({response, body});
+        })
+    }))
+}
