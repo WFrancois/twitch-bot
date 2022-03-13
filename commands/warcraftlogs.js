5c141985a96f82f97b5c61115c9f70a4c90821cd
WFrancois <wroblewski.francois@gmail.com>
2022-03-13 17:45:44 +0100

    update best to sepulcher

--- commands/warcraftlogs.js
+++ commands/warcraftlogs.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 module.exports.getMessage = function () {
     return new Promise((resolve, reject) => {
-        const url = `https://raider.io/api/v1/live-tracking/bossprogress?raid=sanctum-of-domination&difficulty=mythic&region=eu&realm=archimonde&guild=Impact&boss=latest`;
+        const url = `https://raider.io/api/v1/live-tracking/bossprogress?raid=sepulcher-of-the-first-ones&difficulty=mythic&region=eu&realm=archimonde&guild=Impact&boss=latest`;
 
         request(url, (error, response, body) => {
             if (error) {
@@ -12,7 +12,7 @@ module.exports.getMessage = function () {
 
             const bossProgress = JSON.parse(body);
 
-            const raidProgressUrl = `https://raider.io/api/v1/raiding/raid-rankings?raid=sanctum-of-domination&difficulty=mythic&region=eu&realm=archimonde&guilds=861322`;
+            const raidProgressUrl = `https://raider.io/api/v1/raiding/raid-rankings?raid=sepulcher-of-the-first-ones&difficulty=mythic&region=eu&realm=archimonde&guilds=861322`;
 
             request(raidProgressUrl, (rError, rResponse, rBody) => {
                 const raidProgress = JSON.parse(rBody);
@@ -21,7 +21,7 @@ module.exports.getMessage = function () {
 
                 const percentMessage = bossProgress.isDefeated ? 'killed!' : `best pull: ${bossProgress.bestPercent}%,`
 
-                return resolve(`[Progress Sanctum: ${bossKilled}/10] Actuellement sur ${bossProgress.boss.name} : ${percentMessage} ${bossProgress.pullCount} ${bossProgress.pullCount > 1 ? 'pulls' : 'pull'}. Powered by Raider.IO`);
+                return resolve(`[Progress Sepulcher: ${bossKilled}/10] Actuellement sur ${bossProgress.boss.name} : ${percentMessage} ${bossProgress.pullCount} ${bossProgress.pullCount > 1 ? 'pulls' : 'pull'}. Powered by Raider.IO`);
             });
         })
     });
