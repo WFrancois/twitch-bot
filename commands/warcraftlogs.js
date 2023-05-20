d1a331ef48599be99f3ff058f9ae7d8e6fcc56ae
WFrancois <wroblewski.francois@gmail.com>
2023-05-20 11:40:16 +0000

    update vault

--- commands/warcraftlogs.js
+++ commands/warcraftlogs.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 module.exports.getMessage = function () {
     return new Promise((resolve, reject) => {
-        const url = `https://raider.io/api/v1/live-tracking/bossprogress?raid=sepulcher-of-the-first-ones&difficulty=mythic&region=eu&realm=archimonde&guild=Impact&boss=latest`;
+        const url = `https://raider.io/api/v1/live-tracking/bossprogress?raid=vault-of-the-incarnates&difficulty=heroic&region=eu&realm=ysondre&guild=J%20J%20J%20J%20J%20J%20J%20J%20J%20J%20J%20J&boss=latest`;
 
         request(url, (error, response, body) => {
             if (error) {
@@ -12,16 +12,16 @@ module.exports.getMessage = function () {
 
             const bossProgress = JSON.parse(body);
 
-            const raidProgressUrl = `https://raider.io/api/v1/raiding/raid-rankings?raid=sepulcher-of-the-first-ones&difficulty=mythic&region=eu&realm=archimonde&guilds=861322`;
+            const raidProgressUrl = `https://raider.io/api/v1/raiding/raid-rankings?raid=vault-of-the-incarnates&difficulty=heroic&region=eu&realm=archimonde&guilds=1839426`;
 
             request(raidProgressUrl, (rError, rResponse, rBody) => {
                 const raidProgress = JSON.parse(rBody);
 
-                const bossKilled = raidProgress.raidRankings[0].encountersDefeated.length;
+                const bossKilled = raidProgress.raidRankings[0] ? raidProgress.raidRankings[0].encountersDefeated.length : 0
 
                 const percentMessage = bossProgress.isDefeated ? 'killed!' : `best pull: ${bossProgress.bestPercent}%,`
 
-                return resolve(`[Progress Sepulcher: ${bossKilled}/11] Actuellement sur ${bossProgress.boss.name} : ${percentMessage} ${bossProgress.pullCount} ${bossProgress.pullCount > 1 ? 'pulls' : 'pull'}. Powered by Raider.IO`);
+                return resolve(`[Progress Vault: ${bossKilled}/8] Actuellement sur ${bossProgress.boss.name} : ${percentMessage} ${bossProgress.pullCount} ${bossProgress.pullCount > 1 ? 'pulls' : 'pull'}. Powered by Raider.IO`);
             });
         })
     });
