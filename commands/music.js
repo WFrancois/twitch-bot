2336d1dd91ab9b8afce5b6b0a9b85521ca708e07
WFrancois <wroblewski.francois@gmail.com>
2023-05-20 14:02:08 +0200

    add uuids

--- commands/music.js
+++ commands/music.js
@@ -3,6 +3,8 @@ const request = require('request');
 const uuidMelody = {
     kusa: 'ab36ba29-3b36-48e4-9536-b64c711496a5',
     lapi: 'dcbeaa1f-3ee6-459e-a890-2fedd9f0275e',
+    maystine: 'acddbba4-3122-46da-9aad-6ee53d393c2d',
+    batsii__: '9fe1878b-5d18-426d-b5a5-5dad9f76575b',
 };
 
 module.exports.getMessage = async function (channel) {
